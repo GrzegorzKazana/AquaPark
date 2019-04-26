72e2804c9cdbc2e981515561aeb3db80652a7ffd
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-26 14:32:57 +0200

    fixed icons

--- src/components/UserContent/NavBar/NavBar.jsx
+++ src/components/UserContent/NavBar/NavBar.jsx
@@ -1,37 +1,47 @@
 import React from "react";
-import { Menu, Icon } from "antd";
+import styles from "./NavBar.module.scss";
+import { Menu, Dropdown, Icon } from "antd";
 
-const NavBar = () => (
-  <Menu mode="horizontal" theme="dark">
-    <Menu.Item key="mail">
-      <Icon type="mail" />
-      Witaj
+const DropdownMenuOverlay = () => (
+  <Menu>
+    <Menu.Item key="1">
+      <Icon type="question-circle" />
+      FAQ
     </Menu.Item>
-    <Menu.SubMenu
-      title={
-        <span>
-          <Icon type="mail" />
-          Atrakcje
-        </span>
-      }
-    >
-      <Menu.Item>Strefa Basenów</Menu.Item>
-      <Menu.Item>Strefa Saun</Menu.Item>
-      <Menu.Item>Strefa Spa</Menu.Item>
-    </Menu.SubMenu>
-    <Menu.SubMenu
-      title={
-        <span>
-          <Icon type="mail" />
-          Cennik
-        </span>
-      }
-    >
-      <Menu.Item>Cennik Basenów</Menu.Item>
-      <Menu.Item>Cennik Saun</Menu.Item>
-      <Menu.Item>Cennik Spa</Menu.Item>
-      <Menu.Item>Cennik karnetów</Menu.Item>
-    </Menu.SubMenu>
+    <Menu.Item key="2">
+      <Icon type="login" />
+      Log in
+    </Menu.Item>
+    <Menu.Item key="3">
+      <Icon type="user-add" />
+      Sign up
+    </Menu.Item>
   </Menu>
 );
+
+const NavBar = ({ setOpenPage }) => {
+  return (
+    <>
+      <Menu mode="horizontal" theme="dark" onClick={e => setOpenPage(e.key)}>
+        <Menu.Item key="0">
+          <Icon type="info-circle" />
+          Witaj
+        </Menu.Item>
+        <Menu.Item key="1">
+          <Icon type="smile" />
+          Atrakcje
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="dollar" />
+          Cennik
+        </Menu.Item>
+      </Menu>
+      <Dropdown overlay={<DropdownMenuOverlay />} className={styles.DropDown}>
+        <a className="ant-dropdown-link">
+          <Icon type="user" />
+        </a>
+      </Dropdown>
+    </>
+  );
+};
 export default NavBar;
--- src/components/UserContent/index.js
+++ src/components/UserContent/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./UserContent.module.scss";
 import NavBar from "./NavBar/NavBar";
 import WelcomePage from "./WelcomePage/WelcomePage";
@@ -6,16 +6,19 @@ import AreasPage from "./AreasPage/AreasPage";
 import PricingPage from "./PricingPage/PricingPage";
 import { Layout } from "antd";
 
-const UserContent = () => (
-  <Layout className={styles.Layout}>
-    <Layout.Header className={styles.Header}>
-      <NavBar />
-    </Layout.Header>
-    <Layout.Content className={styles.Content}>
-      {/* <WelcomePage /> */}
-      {/* <AreasPage /> */}
-      <PricingPage />
-    </Layout.Content>
-  </Layout>
-);
+const UserContent = () => {
+  const [openPage, setOpenPage] = useState("0");
+  return (
+    <Layout className={styles.Layout}>
+      <Layout.Header className={styles.Header}>
+        <NavBar setOpenPage={setOpenPage} />
+      </Layout.Header>
+      <Layout.Content className={styles.Content}>
+        {openPage === "0" && <WelcomePage />}
+        {openPage === "1" && <AreasPage />}
+        {openPage === "2" && <PricingPage />}
+      </Layout.Content>
+    </Layout>
+  );
+};
 export default UserContent;
