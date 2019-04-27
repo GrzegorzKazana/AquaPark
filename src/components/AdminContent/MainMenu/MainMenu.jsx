c3dacb3234864ae15c1c25cc84c205531c15bb7b
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-27 13:24:32 +0200

    hooked up admin menu with views

--- src/components/AdminContent/MainMenu/MainMenu.jsx
+++ src/components/AdminContent/MainMenu/MainMenu.jsx
@@ -1,49 +1,48 @@
 import React from "react";
-import { Menu, Icon, Switch } from "antd";
+import { Menu, Icon } from "antd";
 
-const MainMenu = () => (
-  <Menu
-    theme="light"
-    //   onClick={this.handleClick}
-    mode="inline"
-  >
-    <Menu.Item key="1">
-      <Icon type="fund" />
-      Obłożenie
-    </Menu.Item>
-    <Menu.SubMenu
-      key="sub1"
-      title={
-        <span>
-          <Icon type="dollar" />
-          <span>Cennik</span>
-        </span>
-      }
-    >
-      <Menu.Item key="2">Ceny</Menu.Item>
-      <Menu.Item key="3">Zniżki periodyczne</Menu.Item>
-      <Menu.Item key="4">Zniżki klas</Menu.Item>
-    </Menu.SubMenu>
-    <Menu.SubMenu
-      key="sub2"
-      title={
-        <span>
-          <Icon type="book" />
-          <span>Słowniki</span>
-        </span>
-      }
-    >
-      <Menu.Item key="5">Słownik A</Menu.Item>
-      <Menu.Item key="6">Słownik B</Menu.Item>
-    </Menu.SubMenu>
-    <Menu.Item key="7">
-      <Icon type="mail" />
-      Newsletter
-    </Menu.Item>
-    <Menu.Item key="8">
-      <Icon type="file-done" />
-      Raporty
-    </Menu.Item>
-  </Menu>
-);
+const MainMenu = ({ setOpenPage, views }) => {
+  const onClick = item => setOpenPage(item.key);
+  return (
+    <Menu theme="light" mode="inline" onClick={onClick}>
+      <Menu.Item key={views.OCCUPACY}>
+        <Icon type="fund" />
+        Obłożenie
+      </Menu.Item>
+      <Menu.SubMenu
+        key="sub1"
+        title={
+          <span>
+            <Icon type="dollar" />
+            <span>Cennik</span>
+          </span>
+        }
+      >
+        <Menu.Item key={views.PRICES}>Ceny</Menu.Item>
+        <Menu.Item key={views.PERIOD_DISCOUNT}>Zniżki periodyczne</Menu.Item>
+        <Menu.Item key={views.CLASS_DISCOUNT}>Zniżki klas</Menu.Item>
+      </Menu.SubMenu>
+      <Menu.SubMenu
+        key="sub2"
+        title={
+          <span>
+            <Icon type="book" />
+            <span>Słowniki</span>
+          </span>
+        }
+      >
+        <Menu.Item key={views.DICT_A}>Słownik A</Menu.Item>
+        <Menu.Item key={views.DICT_B}>Słownik B</Menu.Item>
+      </Menu.SubMenu>
+      <Menu.Item key={views.NEWSLETTER}>
+        <Icon type="mail" />
+        Newsletter
+      </Menu.Item>
+      <Menu.Item key={views.REPORT}>
+        <Icon type="file-done" />
+        Raporty
+      </Menu.Item>
+    </Menu>
+  );
+};
 export default MainMenu;
--- src/components/AdminContent/index.js
+++ src/components/AdminContent/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import "./AdminContent.scss";
 import styles from "./AdminContent.module.scss";
 import NavBar from "./NavBar/NavBar";
 import MainMenu from "./MainMenu/MainMenu";
@@ -7,21 +8,37 @@ import NewsletterPage from "./NewsletterPage/NewsletterPage";
 import PricesPage from "./Pricing/PricesPage/PricesPage";
 import { Layout } from "antd";
 
-const AdminContent = () => (
-  <Layout className={styles.Layout}>
-    <Layout.Header className={styles.Header}>
-      <NavBar />
-    </Layout.Header>
-    <Layout>
-      <Layout.Sider className={styles.Sider} theme="light">
-        <MainMenu />
-      </Layout.Sider>
-      <Layout.Content className={styles.Content}>
-        {/* <OccupancyPage /> */}
-        {/* <NewsletterPage /> */}
-        <PricesPage />
-      </Layout.Content>
+const views = {
+  OCCUPACY: "0",
+  PRICES: "1",
+  PERIOD_DISCOUNT: "2",
+  CLASS_DISCOUNT: "3",
+  DICT_A: "4",
+  DICT_B: "5",
+  NEWSLETTER: "6",
+  REPORT: "7"
+};
+
+const AdminContent = () => {
+  const [openPage, setOpenPage] = useState(views.OCCUPACY);
+  return (
+    <Layout className={styles.Layout}>
+      <Layout.Header className={styles.Header}>
+        <NavBar />
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider theme="light" breakpoint="md" collapsedWidth={0}>
+          <div className={styles.SiderContent}>
+            <MainMenu setOpenPage={setOpenPage} views={views} />
+          </div>
+        </Layout.Sider>
+        <Layout.Content className={styles.Content}>
+          {openPage === views.OCCUPACY && <OccupancyPage />}
+          {openPage === views.NEWSLETTER && <NewsletterPage />}
+          {openPage === views.PRICES && <PricesPage />}
+        </Layout.Content>
+      </Layout>
     </Layout>
-  </Layout>
-);
+  );
+};
 export default AdminContent;
