edf5303ccf586e0b690f6d633a8edf9fbb9d9664
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-05-04 21:31:57 +0200

    fixed overlay blocking elements

--- src/components/Common/SpinnerOverlay/SpinnerOverlay.jsx
+++ src/components/Common/SpinnerOverlay/SpinnerOverlay.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import styles from "./SpinnerOverlay.module.scss";
 import { Spin } from "antd";
 
-const SpinnerOverlay = ({ open }) => (
-  <div className={styles.SpinnerOverlay}>
-    <Spin spinning={open} size="large" />
-  </div>
-);
+const SpinnerOverlay = ({ open }) =>
+  open ? (
+    <div className={styles.SpinnerOverlay}>
+      <Spin spinning={open} size="large" />
+    </div>
+  ) : null;
 export default SpinnerOverlay;
